0396c96375076075d29ff3b16f76c5876fc6bd8b
sm-saikat <mail.smsaikat@gmail.com>
2024-03-29 13:52:02 +0600

    done

--- app/(dashboard)/courses/page.jsx
+++ app/(dashboard)/courses/page.jsx
@@ -1,25 +1,34 @@
+'use client';
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import CourseCard from "@/components/CourseCard";
-import Course from "@/models/Course";
-import { getServerSession } from "next-auth";
-import { headers } from "next/headers";
 import { PlusCircle } from "react-bootstrap-icons";
+import {useSession} from "next-auth/react";
+import { useEffect, useState } from "react";
+import withSession from "@/components/HOC/withSession";
 
-const Courses = async () => {
-    const session = await getServerSession(authOptions);
+const Courses = () => {
+    const [courses, setCourses] = useState(null);
+
+    const { data: session, status } = useSession();
+    console.log('Session', session);
     const user = session?.user;
 
-    const nextHeaders = headers();
+    const fetchCourses = async () => {
+        const response = await fetch('/api/courses/my-courses', {
+            method: 'GET',
+            credentials: 'include', 
+        });
+        
+        if(response.status === 200) {
+            const result = await response.json();
+            console.log('Courses: ', result);
+            setCourses(result.data);
+        }
+    }
 
-    const response = await fetch(`${process.env.API_BASE_URL}/courses/my-courses`, {
-        method: 'GET',
-        headers: {
-            cookie: nextHeaders.get('cookie')
-        } 
-    });
-    const result = await response.json();
-    const courses = result.data;
+    useEffect(()=> {
+        fetchCourses();
+    }, [])
 
     return (
         <div>
@@ -28,11 +37,11 @@ const Courses = async () => {
 
             <div className="courses grid grid-cols-3 gap-4">
                 {courses?.map((course) => {
-                    return <CourseCard key={course._id} course={course} author={user.role === 'lecturer'} />;
+                    return <CourseCard key={course._id} course={course} author={user?.role === 'lecturer'} />;
                 })}
 
                 {
-                    user.role === 'lecturer' &&
+                    user?.role === 'lecturer' &&
                     <div className="max-w-sm p-10 bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center">
                         <PlusCircle className="mb-2 text-primary cursor-pointer" size={50} />
                         <p className="text-gray-700 font-medium dark:text-gray-400">
@@ -45,4 +54,5 @@ const Courses = async () => {
     )
 }
 
-export default Courses
+export const dynamic = "force-dynamic";
+export default withSession(Courses);
